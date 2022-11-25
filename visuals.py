b57ae0e876409238f8977dca0569a48388020b24
Mihail-Zavorotniy <117147244+Mihail-Zavorotniy@users.noreply.github.com>
2022-11-25 18:53:31 +0300

    merged objects and backgrounds into one file

--- visuals.py
+++ visuals.py
@@ -15,3 +15,15 @@ class ObjectNotInteractable:
 
     def draw(self):
         self.screen.blit(self.sprite, (self.x, self.y))
+
+        
+class Background:
+    def __init__(self, screen, sprite, objects_list, coord=[0,0]):
+        self.screen = screen
+        self.sprite = sprite
+        self.x = coord[0]
+        self.y = coord[1]
+        self.objects_list = objects_list
+
+    def draw(self):
+        self.screen.blit(self.sprite, (self.x, self.y))        
