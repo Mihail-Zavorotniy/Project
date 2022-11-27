cf009db00d6ba375385ba93f9a8df39c857274e3
Mihail-Zavorotniy <117147244+Mihail-Zavorotniy@users.noreply.github.com>
2022-11-28 02:03:20 +0300

    added new constants

--- constants.py
+++ constants.py
@@ -1,5 +1,15 @@
-'''Constants'''
+import pygame
+
 
 FPS = 30
-
-WIDTH, HEIGHT = 900, 700
+WIDTH = 900
+HEIGHT = 700
+left_barrier = 0
+right_barrier = WIDTH
+top_barrier = 0
+bottom_barrier = HEIGHT
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(0xFFFFFF)
+clock = pygame.time.Clock()
+finished = False
+player_starting_coord = [450, 350]
