759b10955621c10b5f5cc4d557df07fb7cbac02c
Mihail-Zavorotniy <117147244+Mihail-Zavorotniy@users.noreply.github.com>
2022-11-29 11:59:03 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,10 +6,9 @@ WIDTH = 900
 HEIGHT = 700
 left_barrier = 0
 right_barrier = WIDTH
-top_barrier = 0
+top_barrier = 200
 bottom_barrier = HEIGHT
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(0xFFFFFF)
 clock = pygame.time.Clock()
 finished = False
 player_starting_coord = [450, 350]
