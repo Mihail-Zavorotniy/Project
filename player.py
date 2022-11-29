98e2eca5e2704368820d41abfa1c1de12671e1ae
Mihail-Zavorotniy <117147244+Mihail-Zavorotniy@users.noreply.github.com>
2022-11-29 10:59:53 +0300

    нормировал скорость игрока

--- player.py
+++ player.py
@@ -17,20 +17,20 @@ class Player:
         self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height / 2
         self.prev_hitbox_x = self.hitbox_x
         self.prev_hitbox_y = self.hitbox_y
-
-    def move(self, direcrion: str):
+        
+    def move(self, direcrion: str, norm=1):
         if not self.moved:
             self.prev_hitbox_x = self.hitbox_x
             self.prev_hitbox_y = self.hitbox_y
         self.moved = True
         if direcrion == 'r':
-            self.hitbox_x += self.v
+            self.hitbox_x += self.v / norm
         if direcrion == 'l':
-            self.hitbox_x -= self.v
+            self.hitbox_x -= self.v / norm
         if direcrion == 'u':
-            self.hitbox_y -= self.v
+            self.hitbox_y -= self.v / norm
         if direcrion == 'd':
-            self.hitbox_y += self.v
+            self.hitbox_y += self.v / norm
 
     def draw(self):
         self.screen.blit(self.sprite,
