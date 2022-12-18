1a8153e54d26f41e2e26697f733ddf59ddbbd06d
Ksenia Mizeva <ksenya.mizeva@gmail.com>
2022-12-18 17:16:19 +0300

    добавлены комментарии

--- visuals.py
+++ visuals.py
@@ -2,7 +2,18 @@ import pygame
 
 
 class Object:
+    '''класс объекта
+    screen - экран, на котором расположен объект
+    current_state - текущее состояние объекта (только для объектов с возможным взаимодействием)
+    sprites - кортеж возможных спрайтов
+    sprite - спрайт, который сейчас отображается на экране
+    x, y - координаты лекого верхнего угла объекта
+    hitbox_sizes - размеры объетов для всех спрайтов
+    hitbox_wigth, hitbox_height - размеры предмета в текущем состоянии
+    hitbox_x, hitbox_y - координаты центра объекта
+    '''
     def __init__(self, screen: pygame.Surface, sprites: list, coord: list, hitbox_sizes):
+        '''Конструктор'''
         self.screen = screen
         self.current_state = 0
         self.sprites = sprites
@@ -16,10 +27,18 @@ class Object:
         self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height/2
 
     def draw(self):
+        '''Отрисовка объекта'''
         self.screen.blit(self.sprite, (self.x, self.y))
 
 
 class ObjectInteractable(Object):
+    '''класс объектов, с которыми можно взаимодействовать
+    наследует класс объектов
+    inter_area - зона вокруг предмета, находясь в которой можно взаимодействовать с объектом
+    pickup - опрееляет, как происходит взаимодействие с объектом. true - можно подобрать, false - меняетс прайт при взаимодействии
+    inventory_sprite - спрайт этого объекта в инветаре
+    required_item - объект, необходимый для взаимодействия с этим объектом
+    '''
     def __init__(self, screen: pygame.Surface, sprites: list, coord: list, hitbox_sizes, pickup: bool, interaction_area=4,
                  inventory_sprite=None, required_item=None):
         super().__init__(screen, sprites, coord, hitbox_sizes)
@@ -34,6 +53,7 @@ class ObjectInteractable(Object):
             self.required_item = required_item
 
     def change_state(self, new_state):
+        '''Изменить состояние объекта'''
         self.current_state = new_state
         self.sprite = self.sprites[self.current_state]
         self.hitbox_width = self.hitbox_sizes[self.current_state][0]
@@ -42,6 +62,14 @@ class ObjectInteractable(Object):
         self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height / 2
 
 class Background:
+    '''класс бэкграунда
+    screen - экран, на котором расположен бэкграунд
+    sprite - спрайт бэкграунда
+    x, y - координаты левого верхнего угла бэкграунда (левый верхний угол экрана)
+    objects - кортеж объектов на бэкграунде, отсортированный по их координате y
+    interactable_objects - кортеж объектов на бэкграунде, с которыми можно взаимодействовать, отсортированный по их координате y
+    all_objects - кортеж всех объектов на бэкграунде
+    '''
     def __init__(self, screen, sprite, objects_list, interactable_objects_list=None, coord=[0,0]):
         self.screen = screen
         self.sprite = sprite
@@ -54,4 +82,5 @@ class Background:
 
 
     def draw(self):
+        '''Отрисовка бэкграунда'''
         self.screen.blit(self.sprite, (self.x, self.y))
