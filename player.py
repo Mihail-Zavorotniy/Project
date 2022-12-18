64f242efa7502338a0befe16cfd7668d070a2cf5
Ksenia Mizeva <ksenya.mizeva@gmail.com>
2022-12-18 21:14:13 +0300

    добавлена анимация движения персонажа

--- player.py
+++ player.py
@@ -9,11 +9,13 @@ class Player:
     sprite - текущий спрайт, который отображается на экране
     x, y - координаты левого верхнего угла игрока
     v - скорость движения
-    moved - true если персонаж сейчас двигается, false - в противном случае
+    moved - true если персонаж двигался в предыдущем фрейме, false - в противном случае
     immobile - true если пользователь взаимодействует в инвентарем и игрока нельзя двигать, false - в противном случае
     hitbox_width, hitbox_height - размеры хитбокса игрока
     hitbox_x, hitbox_y - координаты центра хитбокса
     prev_hitbox_x, prev_hitbox_y - позиция хитбокса на предыдущем фрейме
+    animation - определяет текущий спрайт при движении
+    animation_speed - раз в сколько фреймов меняется спрайт
     '''
     def __init__(self, screen: pygame.Surface, sprites: dict, coord: list,
                   movement_speed, hitbox_size: list):
@@ -32,25 +34,34 @@ class Player:
         self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height / 2
         self.prev_hitbox_x = self.hitbox_x
         self.prev_hitbox_y = self.hitbox_y
+        self.animation = 0
+        self.animation_speed = 3
 
     def move(self, direcrion: str, norm=1):
         '''Движение игрока'''
+        sprite_name = ''
         if not self.moved:
             self.prev_hitbox_x = self.hitbox_x
             self.prev_hitbox_y = self.hitbox_y
+        self.animation = (self.animation + 1) % (6 * self.animation_speed)
+        sprite_number = int((self.animation + 1) / self.animation_speed) % 6 + 1
         self.moved = True
         if direcrion == 'r':
             self.hitbox_x += self.v / norm
-            self.sprite = self.sprites['right1']
+            sprite_name = 'right' + str(sprite_number)
+            self.sprite = self.sprites[sprite_name]
         if direcrion == 'l':
             self.hitbox_x -= self.v / norm
-            self.sprite = self.sprites['left1']
+            sprite_name = 'left' + str(sprite_number)
+            self.sprite = self.sprites[sprite_name]
         if direcrion == 'u':
             self.hitbox_y -= self.v / norm
-            self.sprite = self.sprites['up1']
+            sprite_name = 'up' + str(sprite_number)
+            self.sprite = self.sprites[sprite_name]
         if direcrion == 'd':
             self.hitbox_y += self.v / norm
-            self.sprite = self.sprites['down1']
+            sprite_name = 'down' + str(sprite_number)
+            self.sprite = self.sprites[sprite_name]
 
     def draw(self):
         '''Отрисовка игрока'''
