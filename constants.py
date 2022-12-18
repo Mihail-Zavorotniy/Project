83d12d16b87b05ee4866f8790712eba95be09672
Ksenia Mizeva <ksenya.mizeva@gmail.com>
2022-12-18 13:34:32 +0300

    добавлены комментарии

--- constants.py
+++ constants.py
@@ -2,12 +2,17 @@ import pygame
 
 
 FPS = 30
+
+'''Размеры экрана'''
 WIDTH = 900
 HEIGHT = 700
+
+'''Границы области, по которой может передвигаться персонаж'''
 left_barrier = 0
 right_barrier = WIDTH
 top_barrier = 200
 bottom_barrier = HEIGHT
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 finished = False
