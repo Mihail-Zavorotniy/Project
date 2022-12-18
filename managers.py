5d698bb69e22dca503116be455b3a101f9cd004a
Ksenia Mizeva <ksenya.mizeva@gmail.com>
2022-12-18 15:17:22 +0300

    добавлены комментарии

--- managers.py
+++ managers.py
@@ -2,14 +2,22 @@ from copy import copy
 
 
 class BackgroundManager:
+
+    '''класс BackgroungManager
+    backgrounds - список всех бэкграундов в игре
+    current_bg - бэкграунд локации, в которой надожится персонаж
+    '''
     def __init__(self, backgrounds, current_bg_number):
+        '''Конструктор'''
         self.backgrounds = backgrounds
         self.current_bg = backgrounds[current_bg_number]
 
     def update(self, new_bg_number):
+        '''Изменение бэкграунда'''
         self.current_bg = self.backgrounds[new_bg_number]
 
     def draw_scenery(self, player):
+        '''Отрисовка бэкграунда и объектов в комнате'''
         self.current_bg.draw()
         objects_to_draw = copy(self.current_bg.all_objects)
         player_drawn = False
