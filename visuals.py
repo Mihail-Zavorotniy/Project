72532f70f199c0472a60b635f11639532b3a3472
georg31t <ya.grisha31@yandex.ru>
2022-12-19 06:42:18 +0300

    набросок взаимодействия

--- visuals.py
+++ visuals.py
@@ -38,9 +38,10 @@ class ObjectInteractable(Object):
     pickup - опрееляет, как происходит взаимодействие с объектом. true - можно подобрать, false - меняетс прайт при взаимодействии
     inventory_sprite - спрайт этого объекта в инветаре
     required_item - объект, необходимый для взаимодействия с этим объектом
+    given_item - объект, получаемый после взаимодействия
     '''
     def __init__(self, screen: pygame.Surface, sprites: list, coord: list, hitbox_sizes, pickup: bool, interaction_area=4,
-                 inventory_sprite=None, required_item=None):
+                 inventory_sprite=None, required_item=None, given_item=None ):
         super().__init__(screen, sprites, coord, hitbox_sizes)
         self.inter_area = interaction_area
         self.pickup = pickup
@@ -61,28 +62,20 @@ class ObjectInteractable(Object):
         self.hitbox_x = self.x + self.sprite.get_width() / 2
         self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height / 2
 
-class StaticObjectInteractable(Object):
-    def __init__(self, screen: pygame.Surface, sprites: list, coord: list, hitbox_sizes, pickup: bool, interaction_area=4,
-                 inventory_sprite=None, required_item=None):
-        super().__init__(screen, sprites, coord, hitbox_sizes)
-        self.inter_area = interaction_area
-        self.pickup = pickup
-        if self.pickup:
-            if inventory_sprite != None:
-                self.inventory_sprite = inventory_sprite
-            else:
-                self.inventory_sprite = self.sprite
-        elif required_item != None:
-            self.required_item = required_item
+    def give(self, player: Player, inventory: Inventory):
+    '''
+    дает объекты после взаимодействия
+    '''
+   
+    if ((abs(player.hitbox_x - self.hitbox_x) <= (player.hitbox_width + self.hitbox_width)/2 + self.inter_area) and
+        (abs(player.hitbox_y - self.hitbox_y) <= (player.hitbox_height + self.hitbox_height)/2 + self.inter_area)) and
+        self.required_item == invenory.contents[selected_slot] and player.immobile:
+          
+        inventory.add_item(self.given_item)
+        inventory.remove_item(invenory.contents[selected_slot])
 
-    def change_state(self, new_state):
-        '''Изменить состояние объекта'''
-        self.current_state = new_state
-        self.sprite = self.sprites[self.current_state]
-        self.hitbox_width = self.hitbox_sizes[self.current_state][0]
-        self.hitbox_height = self.hitbox_sizes[self.current_state][1]
-        self.hitbox_x = self.x + self.sprite.get_width() / 2
-        self.hitbox_y = self.y + self.sprite.get_height() - self.hitbox_height / 2
+
+
 
 
 
